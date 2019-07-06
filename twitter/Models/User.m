0dcaec6c97d4852a93bdfba6675834807be2f20a
festusojo123 <37356473+festusojo123@users.noreply.github.com>
2019-07-05 21:40:55 -0700

    Update User.m

--- twitter/Models/User.m
+++ twitter/Models/User.m
@@ -12,6 +12,7 @@
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
     self = [super init];
     if (self) {
+    //these are the different user categories, specific to a user and repeatable for multiple tweets compared to tweet details that are specific to each particular tweet
         self.name = dictionary[@"name"];
         self.screenName = dictionary[@"screen_name"];
         self.profilePic = dictionary[@"profile_image_url_https"];
