e8918f6f31b9b3437fbdb5d600d776fac68f7935
festusojo123 <37356473+festusojo123@users.noreply.github.com>
2019-07-05 21:38:40 -0700

    Update AppDelegate.m

--- twitter/Delegate Files/AppDelegate.m
+++ twitter/Delegate Files/AppDelegate.m
@@ -10,24 +10,23 @@
 #import "APIManager.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
+    //gets calls/info from the API to provide info for Table View
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions { 
+    //checks if API manager is working
     if ([[APIManager shared] isAuthorized]) {
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        // instantiates an instace of View Controller to transmit info from API
         UINavigationController *navigationController = [storyboard instantiateViewControllerWithIdentifier:@"TweetsNavigationController"];
         self.window.rootViewController = navigationController;
     }
-    
     return YES;
 }
 
-
+//the rest of this code was provided for us
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
