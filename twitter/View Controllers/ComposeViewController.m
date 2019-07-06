eb796e664d5405c109eb8868eb81daa193a5765e
festusojo123 <37356473+festusojo123@users.noreply.github.com>
2019-07-05 21:53:55 -0700

    Update ComposeViewController.m

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -16,30 +16,31 @@
 
 @implementation ComposeViewController
 
-
-
-
+//after tweet successfully posts, it goes back to the timeline
 - (IBAction)closeAction:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
-
+//this sends off the tweet
 - (IBAction)tweetAction:(id)sender {
     [[APIManager shared] postStatusWithText:_ComposeTweetView.text completion:^(Tweet *newTweet, NSError *invalidText) {
+        //this condition only occurs if it is successful, ands adds the new tweet to the array later
         if (newTweet) {
             NSLog(@"tweet success");
             [self.delegate didTweet:newTweet];
         }
+        //if it fails, it will return the error and get into this condition
         else {
             NSLog(@"tweet failed %@", invalidText.localizedDescription);
         }
+        //dismisses the page back
         [self dismissViewControllerAnimated:true completion:nil];
     }];
 }
 
+//calls itself to make sure page loads
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 /*
